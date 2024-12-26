e843f97fc53d5113c8d99c509418ef127af7ed47
ecroft2 <ecroft2@hotmail.com>
2024-12-26 09:50:34 +0000

    Add firebase to project

---
+++ firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC2itCigDHlmdbMtqvMrTDpD7hUF0up6is",
+  authDomain: "sent-14b97.firebaseapp.com",
+  projectId: "sent-14b97",
+  storageBucket: "sent-14b97.firebasestorage.app",
+  messagingSenderId: "359685144366",
+  appId: "1:359685144366:web:a860c8add369d46dd384bf"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
