88114ec22d1b5048eb2d2d5ba48e61ecbfa7e761
ecroft2 <ecroft2@hotmail.com>
2025-02-23 17:26:15 +0000

    Create EmailInput field and validation hook

--- src/components/EmailInput.jsx
+++ src/components/EmailInput.jsx
@@ -1,51 +1,79 @@
 import { useState, useEffect } from "react";
 
-import useValidateEmail from "../hooks/useValidateEmail";
+import useValidateEmail from "../hooks/useClientValidateEmail";
 
-const EmailInput = (props) => {
+const EmailInput = ({
+    fieldValidationStatus,
+    id,
+    name,
+    required,
+    serverValidationStatus,
+}) => {
     const [value, setValue] = useState("");
     const [hasInteracted, setHasInteracted] = useState(false);
 
-    const { validationResponse, validateInput } = useValidateEmail(
-        props.required
-    );
+    const { validationStatus: clientValidationStatus, validateInput } =
+        useValidateEmail(required);
 
     useEffect(() => {
         if (hasInteracted) {
-            validateInput(value);
+            if (
+                serverValidationStatus?.error === true ||
+                clientValidationStatus?.error === true
+            ) {
+                validateInput(value);
+            }
         }
-    }, [value, validateInput, hasInteracted]);
+    }, [
+        value,
+        validateInput,
+        hasInteracted,
+        serverValidationStatus,
+        clientValidationStatus,
+    ]);
 
     useEffect(() => {
-        if (validationResponse) {
-            props.isValid(validationResponse.isValid);
+        if (fieldValidationStatus) {
+            fieldValidationStatus({
+                field: "email",
+                error:
+                    serverValidationStatus?.error === true ||
+                    clientValidationStatus?.error === true,
+            });
         }
-    }, [props, validationResponse]);
+    }, [fieldValidationStatus, serverValidationStatus, clientValidationStatus]);
 
     return (
         <div>
-            <label htmlFor={props.id}>Email</label>
-
+            <label htmlFor={id}>Email</label>
             <input
-                id={props.id}
-                name={props.name}
+                id={id}
+                name={name}
                 onChange={(event) => {
                     setValue(event.target.value);
                 }}
                 onBlur={(event) => {
-                    validateInput(event.target.value);
                     setHasInteracted(true);
+                    validateInput(event.target.value);
                 }}
-                required={props.required}
+                required={required}
                 type="email"
                 value={value}
             ></input>
 
-            {validationResponse?.errorType === "required_field" &&
-                "This field is required."}
+            {clientValidationStatus?.errorCode === "required_field" ||
+                (serverValidationStatus?.errorCode === "auth/missing-email" &&
+                    "Email is required.")}
 
-            {validationResponse?.errorType === "invalid_format" &&
+            {clientValidationStatus?.errorCode === "invalid_format" &&
                 "This field is in the incorrect format."}
+
+            {serverValidationStatus?.errorCode ===
+                "auth/email-already-in-use" &&
+                "This email is already registered. You can sign in."}
+
+            {serverValidationStatus?.errorCode === "auth/invalid-email" &&
+                "Check your email is formatted correctly and try again."}
         </div>
     );
 };
