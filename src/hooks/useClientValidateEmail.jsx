17d15719149470d26039d30d5b997bbaac932a6d
ecroft2 <ecroft2@hotmail.com>
2025-02-24 20:53:51 +0000

    Move setValidationStatus into each conditional block and return

--- src/hooks/useClientValidateEmail.jsx
+++ src/hooks/useClientValidateEmail.jsx
@@ -8,16 +8,21 @@ const useClientValidateEmail = (isRequired) => {
             const formattingRegex =
                 /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\;[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
-            let status = {
-                error: false,
-                errorCode: null,
-            };
+            let status = { error: false, errorCode: null };
 
             if (input.length === 0 && isRequired) {
                 status = {
                     error: true,
                     errorCode: "required_field",
                 };
+
+                setValidationStatus((prevState) =>
+                    prevState?.error === status.error &&
+                    prevState?.errorCode === status.errorCode
+                        ? prevState
+                        : status
+                );
+                return;
             }
 
             if (formattingRegex.test(input) === false) {
@@ -25,10 +30,19 @@ const useClientValidateEmail = (isRequired) => {
                     error: true,
                     errorCode: "invalid_format",
                 };
+
+                setValidationStatus((prevState) =>
+                    prevState?.error === status.error &&
+                    prevState?.errorCode === status.errorCode
+                        ? prevState
+                        : status
+                );
+                return;
             }
 
             setValidationStatus((prevState) =>
-                JSON.stringify(prevState) === JSON.stringify(status)
+                prevState?.error === status.error &&
+                prevState?.errorCode === status.errorCode
                     ? prevState
                     : status
             );
